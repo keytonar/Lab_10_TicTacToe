59affa19f295712a2157007b203b61f68335a2c3
keytonar <austinkeyton@gmail.com>
2023-04-11 09:05:11 -0400

    Add private classes for each condition

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,51 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class TicTacToe {
+    private static final int ROW = 3;
+    private static final int COL = 3;
+    private static String[][] board = new String[ROW][COL];
+
+    public static void main(String[] args)
+    {
+
+    }
+
+    private static void clearBoard()
+    {
+
+    }
+
+    private static void display()
+    {
+
+    }
+
+    private static boolean isValidMove(int row, int col)
+    {
+
+    }
+
+    private static boolean isWin(String player)
+    {
+
+        return isColWin(player) || isRowWin(player) || isDiagonalWin(player);
+    }
+
+    private static boolean isColWin(String player)
+    {
+
+    }
+
+    private static boolean isRowWin(String player)
+    {
+
+    }
+
+    private static boolean isDiagonalWin(String player)
+    {
+
+    }
+
+    private static boolean isTie()
+    {
+
     }
 }
